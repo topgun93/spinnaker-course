d62c17705ca1cafca04c56d8b733b1e168ffc60a
topgun93 <38679840+topgun93@users.noreply.github.com>
2019-05-16 17:27:29 +0530

    update to enable AWS configuration

--- scripts/7-configure-aws.sh
+++ scripts/7-configure-aws.sh
@@ -4,5 +4,7 @@ hal config provider aws account add $1 \
   --account-id $2 \
   --assume-role role/spinnakerManaged
   
-echo $4 | hal config provider aws edit --access-key-id $ 3 \
+echo $4 | hal config provider aws edit --access-key-id $3 \
   --secret-access-key
+
+hal config provider aws enable
